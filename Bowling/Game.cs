7dc07c57aa2d209a02f1593da65d1d7a6378d3f3
Arnav Limaye <aal4493@gmail.com>
2014-07-15 18:50:07 +0530

    Committing 1st version of Bowling Code with test cases.
    5 test cases are working.
    1 test case(AllStrikes) is failing.

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,15 +8,72 @@ namespace Bowling
 {
     public class Game
     {
+        private int _score;
+        private List<int> _rolls;
+
+        public Game()
+        {
+            _score = 0;
+            _rolls = new List<int>();
+        }
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            _rolls.Add(pins);
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int bonus = 0;
+            int remaining = 20;
+            int current;
+            bool strike = false;
+            int[] frame = new int[2];
+            for (current = 0; current < _rolls.Count; current++)
+            {
+                
+                if (_rolls[current] == 10 && remaining > 0) 
+                {
+                    strike = false;    
+                    StikeOrSpare(_rolls, current, ref strike);
+                    if (strike == true)
+                    {
+                        bonus += _rolls[current + 1] + _rolls[current + 2] + _rolls[current];
+                        remaining -= 2;
+                        if (remaining <= 0)
+                            break;
+                    }
+                    else
+                    {
+                        bonus += _rolls[current + 1] + _rolls[current];
+                        remaining -= 1;
+                    }
+                }
+                else
+                {
+                    _score += _rolls[current];
+                    remaining--;
+                }
+            }
+            for (current = 0; current < _rolls.Count-1; current += 2)
+            {
+                if ((_rolls[current] + _rolls[current + 1]) == 10 && _rolls[current] != 10)
+                {
+                    bonus += _rolls[current + 2];
+                }
+            }
+                _score += bonus;
+            return _score;
         }
+
+        private void StikeOrSpare(List<int> _rolls, int current, ref bool strike)
+        {
+            if (current % 2 == 0)
+                strike = true;
+            else
+                strike = false;
+        }
+
+        
 
     }
 }
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bowling;
 
 namespace BowlingFixtures
 {
@@ -7,8 +8,165 @@ namespace BowlingFixtures
     public class GameFixture
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AllStrikes()
         {
+            Game g = new Game();
+            for (int i = 0; i < 12; i++)
+            {
+                g.Roll(10);
+            }
+            Assert.AreEqual(300, g.GetScore());
         }
+
+        [TestMethod]
+        public void NoStrikeOrSpare()
+        {
+            Game g = new Game();
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(0);
+            g.Roll(3);
+            g.Roll(0);
+            g.Roll(3);
+
+            Assert.AreEqual(30, g.GetScore());
+        }
+
+        [TestMethod]
+        public void StrikeOrSpareInBetween()
+        {
+            Game g = new Game();
+            g.Roll(7);
+            g.Roll(3);
+            
+            g.Roll(2);
+            g.Roll(5);
+            
+            g.Roll(6);
+            g.Roll(2);
+            
+            g.Roll(0);
+            g.Roll(7);
+
+            g.Roll(3);
+            g.Roll(7);
+
+            g.Roll(6);
+            g.Roll(2);
+
+            g.Roll(3);
+            g.Roll(2);
+            
+            g.Roll(6);
+            g.Roll(2);
+
+            g.Roll(10);
+
+            g.Roll(0);
+            g.Roll(0);
+            Assert.AreEqual(81, g.GetScore());
+        }
+
+        [TestMethod]
+        public void SpareAtLast()
+        {
+            Game g = new Game();
+            g.Roll(7);
+            g.Roll(3);
+
+            g.Roll(2);
+            g.Roll(5);
+
+            g.Roll(6);
+            g.Roll(2);
+
+            g.Roll(0);
+            g.Roll(7);
+
+            g.Roll(3);
+            g.Roll(7);
+
+            g.Roll(6);
+            g.Roll(2);
+
+            g.Roll(3);
+            g.Roll(2);
+
+            g.Roll(6);
+            g.Roll(2);
+
+            g.Roll(10);
+
+            g.Roll(7);
+            g.Roll(3);
+
+            g.Roll(4);
+            Assert.AreEqual(105, g.GetScore());
+        }
+
+        [TestMethod]
+        public void AllGutters()
+        {
+            Game g = new Game();
+            for (int i = 0; i < 10; i++)
+            {
+                g.Roll(0);
+            }
+            Assert.AreEqual(0, g.GetScore());
+        }
+
+        [TestMethod]
+        public void StrikeAtLast()
+        {
+            Game g = new Game();
+            g.Roll(7);
+            g.Roll(3);
+
+            g.Roll(2);
+            g.Roll(5);
+
+            g.Roll(6);
+            g.Roll(2);
+
+            g.Roll(0);
+            g.Roll(7);
+
+            g.Roll(3);
+            g.Roll(7);
+
+            g.Roll(6);
+            g.Roll(2);
+
+            g.Roll(3);
+            g.Roll(2);
+
+            g.Roll(6);
+            g.Roll(2);
+
+            g.Roll(7);
+            g.Roll(2);
+
+            g.Roll(10);
+
+            g.Roll(4);
+            g.Roll(6);
+
+            Assert.AreEqual(100, g.GetScore());
+        }
+
     }
 }
